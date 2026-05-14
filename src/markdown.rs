e7a30c6ede4a73764f1abb60b28d34b6f660d069
kgv <kgv@users.noreply.github.com>
2026-05-14 13:08:55 +0300

    Fix scroll

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,4 +1,4 @@
-use egui::{Id, Image, ImageSource, Rgba, Ui, load::Bytes, mutex::Mutex};
+use egui::{Id, Image, ImageSource, Rgba, ScrollArea, Ui, load::Bytes, mutex::Mutex};
 use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
 use std::{collections::HashMap, sync::Arc};
 
@@ -25,24 +25,28 @@ impl Markdown for Ui {
                 .clone()
             }),
         );
-        CommonMarkViewer::new()
-            .render_math_fn(Some(&move |ui, math, inline| {
-                let mut cache = cache.0.lock();
-                let color = ui.visuals().strong_text_color();
-                let svg = cache
-                    .entry(math.to_string())
-                    .or_insert_with(|| render_math(math, inline, color.into()));
-                let uri = format!("{}.svg", Id::from(math.to_string()).value());
-                let mut image = Image::new(ImageSource::Bytes {
-                    uri: uri.into(),
-                    bytes: Bytes::Shared(svg.clone()),
-                });
-                if !inline {
-                    image = image.fit_to_original_size(0.5);
-                }
-                ui.add(image);
-            }))
-            .show_scrollable(self.next_auto_id(), self, &mut cache.1.lock(), markdown)
+        ScrollArea::vertical().show(self, |ui| {
+            CommonMarkViewer::new()
+                .render_math_fn(Some(&move |ui, math, inline| {
+                    let mut cache = cache.0.lock();
+                    let color = ui.visuals().strong_text_color();
+                    let svg = cache
+                        .entry(math.to_string())
+                        .or_insert_with(|| render_math(math, inline, color.into()));
+                    let uri = format!("{}.svg", Id::from(math.to_string()).value());
+                    let mut image = Image::new(ImageSource::Bytes {
+                        uri: uri.into(),
+                        bytes: Bytes::Shared(svg.clone()),
+                    });
+                    if !inline {
+                        image = image.fit_to_original_size(0.5);
+                    }
+                    ui.add(image);
+                }))
+                .show(ui, &mut cache.1.lock(), markdown)
+        });
+        // TODO: https://github.com/lampsitter/egui_commonmark/issues/87
+        // .show_scrollable(self.next_auto_id(), self, &mut cache.1.lock(), markdown)
     }
 }
 
